25a38016c988380277e5e8d17402f3c0d4186949
zouhangte <zouhangtezbm@126.com>
2016-08-09 09:02:24 +0800

    信息名换回为字符串

--- Const/ObserverName.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public enum ObserverName:int{
-	green,black,red
-}
---
+++ PureMVC/Core/SimpleCommond.cs
@@ -0,0 +1,9 @@
+﻿
+public abstract class Command : Notifyer, ICommand
+{
+	public abstract void Execute(INotification<object> notification);
+}
+public abstract class Command<T> : Notifyer, ICommand<T>
+{
+	public abstract void Execute(INotification<T> notification);
+}
