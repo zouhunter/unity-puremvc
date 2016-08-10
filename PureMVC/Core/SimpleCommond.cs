e01a23db5ce516906dcd0c228db8de1838abfe45
zouhangte <zouhangtezbm@126.com>
2016-08-10 15:53:53 +0800

    删除多余文件

--- PureMVC/Core/SimpleCommond.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public abstract class Command : Notifyer, ICommand
-{
-	public abstract void Execute(INotification<object> notification);
-}
-public abstract class Command<T> : Notifyer, ICommand<T>
-{
-	public abstract void Execute(INotification<T> notification);
-}
--- PureMVC/Interface/IController.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public interface IController {
-	void RegisterCommand(string commandName,ICommand command);
-	ICommand RemoveCommand(string commandName);
-	bool HasCommand(string commandName);
-}
